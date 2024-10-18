aff6f5a2b43a495f4eaefa5d2341854aa4a4beb0
Labzin <lbzin@bk.ru>
2024-10-19 00:19:37 +0300

    KS-1-5 fix magic number and add String.format

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,9 +1,15 @@
 package org.example.lesson_1
 
-fun main () {
+const val minInHour: Int = 60
+const val secInMin: Int = 60
+const val secInHour: Int = 3600
+
+fun main() {
+
     val seconds: Int = 6480
-    val hours: Int = seconds/3600
-    val minute: Int = (seconds % (hours*3600))/60
-    val sec: Int = seconds-hours*3600-minute*60
-    println("0"+ hours + ":" + minute+":0" + sec)
+    val hours: Int = seconds / secInHour
+    val minute: Int = (seconds % (hours * secInHour)) / minInHour
+    val sec: Int = seconds - hours * secInHour - minute * secInMin
+    val info = String.format("0%d:%2d:0%d", hours, minute, sec);
+    println(info)
 }
