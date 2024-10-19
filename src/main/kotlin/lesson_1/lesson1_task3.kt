9c7d1faecb584775df054c83df86b426bb027ace
Labzin <lbzin@bk.ru>
2024-10-19 12:01:51 +0300

    KS-1-3 fix hour and minutes to String

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,20 +2,18 @@ package org.example.lesson_1
 
 fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
-    val realHour = String.format("0%d", hour)
-    val realMinute = String.format("0%d", minute)
 
     println(year)
-    println(realHour)
-    println(realMinute)
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
-    val andTime = String.format("%d:%d", hour, minute)
+    val andTime = String.format("%s:%s", hour, minute)
 
     print(andTime)
 }
