1f8782b8b0c9de0139da43355b72bf49cc10d840
Labzin <lbzin@bk.ru>
2024-10-23 20:08:41 +0300

    KS-3-1 good day

---
+++ src/main/kotlin/lesson_3/Lesson3_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_3
+
+fun main() {
+    var userName: String = "новый пользователь"
+    var needSay: String
+
+    needSay = "Дорое утро!, $userName"
+    println(needSay)
+    needSay = "Добрый день!, $userName"
+    println(needSay)
+}
