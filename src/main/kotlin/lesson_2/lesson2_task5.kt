304a952ea78e185f07a86bfdf507be26470c9e66
Labzin <lbzin@bk.ru>
2024-10-25 17:18:49 +0300

    KS-3-5 chess2

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val firstMoney: Int = 70_000
+    val persents: Double = 16.7
+    val time: Int = 20
+
+    val moneyAfter = firstMoney*((1+persents/100)).pow(time)
+
+    println(String.format("%.3f",moneyAfter))
+
+}
