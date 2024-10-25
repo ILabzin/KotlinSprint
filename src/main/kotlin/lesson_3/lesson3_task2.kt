7395d1f65255df20642a776e3eeea50f9742bbca
Labzin <lbzin@bk.ru>
2024-10-25 13:11:28 +0300

    KS-3-2 women surname

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var name: String = "Татьяна"
+    var oldSurname: String = "Андреева"
+    var newSurname: String = "Сидорова"
+    var patronymic: String = "Сергеевна"
+
+    println("$oldSurname $name $patronymic, 20 лет")
+    println("$newSurname $name $patronymic, 22 года")
+
+}
