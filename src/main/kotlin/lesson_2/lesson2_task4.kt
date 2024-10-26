3f3ee1012a2e6c00ffad05ab817f9a0ceb5de540
Labzin <lbzin@bk.ru>
2024-10-26 19:41:19 +0300

    KS-2-4 fix magic number

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
 const val buff: Int = 20
+const val persent: Int = 100
 
 fun main() {
     val crystal: Int = 7
     val iron: Int = 11
 
-    val crystalBuff: Int = (crystal * (buff/100))
-    val ironBuff: Int = (iron * (buff/100))
+    val crystalBuff: Int = (crystal * (buff / persent))
+    val ironBuff: Int = (iron * (buff / persent))
 
     println("Бонусная кристальная руда: " + crystalBuff)
     println("Бонусная железная руда: " + ironBuff)
