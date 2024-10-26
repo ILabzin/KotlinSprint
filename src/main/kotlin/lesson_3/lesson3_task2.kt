645aac3db4a7771adc357aef5af27227c2408a9f
Labzin <lbzin@bk.ru>
2024-10-26 16:05:48 +0300

    KS-3-2 fix surname, fix val\var

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,12 +1,21 @@
 package org.example.lesson_3
 
 fun main() {
-    var name: String = "Татьяна"
-    var oldSurname: String = "Андреева"
-    var newSurname: String = "Сидорова"
-    var patronymic: String = "Сергеевна"
+    val name: String = "Татьяна" //var до этого принял решение тут использовать, если человек захочет сменить имя
+    var Surname: String = "Андреева"
+    val patronymic: String = "Сергеевна"
+    var age = 20
 
-    println("$oldSurname $name $patronymic, 20 лет")
-    println("$newSurname $name $patronymic, 22 года")
+    //var newSurname: String = "Сидорова" я предположил, что старую фамилию тоже
+    //стоит хранить в отдельной переменной и не "затирать". Для базы это хорошо, какое-то
+    //обращение или запрос может быть по старой фамилии
+
+    println("$Surname $name $patronymic, $age")
+
+    Surname = "Сидорова"
+    age = 22
+
+
+    println("$Surname $name $patronymic, $age")
 
 }
