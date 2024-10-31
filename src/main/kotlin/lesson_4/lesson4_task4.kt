fa7d838625a57231fb46ada6361307b388ce42cb
Labzin <lbzin@bk.ru>
2024-10-31 20:30:35 +0300

    KS-4-4 goodStyle

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,16 +2,16 @@ package org.example.lesson_4
 
 
 fun day(int: Int) {
-    val flag:Boolean = (int % 2 == 0)
+    val flag: Boolean = (int % 2 == 0)
 
-        println(
-            """
+    println(
+        """
                 Упражнения для рук:    ${!flag}
                 Упражнения для ног:    ${flag}
                 Упражнения для спины:  ${flag}
                 Упражнения для пресса: ${!flag}                
             """.trimIndent()
-        )
+    )
 }
 
 fun main() {
