8d22836c4515d55ffedc051da2450927336b2ecd
Labzin <lbzin@bk.ru>
2024-11-02 22:10:16 +0300

    KS-7-2 убрал создание диапазона из цикла

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_7
 
 fun main() {
+    val diapazon = 1000..9999
+    var peopleNumber: Int
     var number:Int
-    var peopleNumber:Int
 
     while (true) {
-        number= (1000..9999).random()
+        number=diapazon.random()
         println("Ваш код авторизации: $number")
         println("Введите код авторизации")
         peopleNumber = readln().toInt()
-        if (peopleNumber==number) break
+        if (peopleNumber == number) break
 
     }
 }
