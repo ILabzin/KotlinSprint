d5b86410c4309f30a242b16aa6080948ff42c58d
Labzin <lbzin@bk.ru>
2024-11-14 01:17:15 +0300

    KS-7-1 add 2 range

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -4,16 +4,20 @@ fun main() {
 
     var password: String = ""
     var symbol: String
-    val passwordLength = 6
+    val numberDia = 48..57
+    val wordsDia = 97..122
     var number: Int
 
-    while (password.length < passwordLength) {
-        number = (48..122).random()
-        if (number in 58..96) {
-            continue
+    for (i in 1..6) {
+        number = if ((i % 2) == 0) wordsDia.random()
+        else {
+            numberDia.random()
         }
+
+
         symbol = number.toChar().toString()
         password += symbol
     }
+
     println(password)
 }
