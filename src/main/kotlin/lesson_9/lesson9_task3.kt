dff1ca9aef45a912014c4be9caaaef0d6255cab1
Иван <Lbzin@bk.ru>
2024-11-14 22:34:28 +0300

    KS-9-3 add map function

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -2,17 +2,15 @@ package org.example.lesson_9
 
 fun main() {
     val arrayForOne = arrayListOf(2, 50, 15)
-    val sumDose: MutableList<Int> = mutableListOf()
 
     println("Сколько порций приготовить?")
     val numberDose = readln().toInt()
 
-    for (i in 0..<arrayForOne.size) {
-        sumDose.add(arrayForOne[i] * numberDose)
-    }
+    var newArray = arrayForOne.map { it * numberDose }
+
     println(
-        "На $numberDose порций вам понадобится: Яиц – ${sumDose[0]}, молока – ${sumDose[1]}," +
-                " сливочного масла – ${sumDose[2]}"
+        "На $numberDose порций вам понадобится: Яиц – ${newArray[0]}, молока – ${newArray[1]}," +
+                " сливочного масла – ${newArray[2]}"
     )
 
 
