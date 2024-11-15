26b1627bf268cb7cf9a77fcfdd4a0778cdb832f0
Иван <Lbzin@bk.ru>
2024-11-15 23:37:06 +0300

    KS-10-3 fix dia in fun

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_10
 
-val number = 0..9
-val symbol = ' '..'/'
-
 fun generatePassword(int: Int): String {
+    val number = 0..9
+    val symbol = ' '..'/'
     var password: String = ""
+
     for (i in 1..int) {
         if (i % 2 == 0) password += number.random()
         else password += symbol.random()
