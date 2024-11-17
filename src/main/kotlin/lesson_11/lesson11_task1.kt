215b5908de477af5d69d36c3369c4e7efe51b48c
Labzin <lbzin@bk.ru>
2024-11-17 18:42:05 +0300

    KS-11-1 class User

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_11
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+) {
+
+}
+
+fun main() {
+    val user1: User = User(1, "Van", "1234", "12@34")
+    val user2: User = User(2, "Van2", "12345", "12@345")
+
+    println("""
+        id: ${user1.id}
+        login: ${user1.login}
+        password: ${user1.password}
+        mail: ${user1.mail}
+    """.trimIndent())
+
+    println("""
+        id: ${user2.id}
+        login: ${user2.login}
+        password: ${user2.password}
+        mail: ${user2.mail}
+    """.trimIndent())
+}
