8e775f60ec3d4d619c89f0d8c4b4ffa4a8bafb30
Labzin <lbzin@bk.ru>
2024-11-17 18:43:48 +0300

    KS-11-1 fix вид

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -13,17 +13,21 @@ fun main() {
     val user1: User = User(1, "Van", "1234", "12@34")
     val user2: User = User(2, "Van2", "12345", "12@345")
 
-    println("""
+    println(
+        """
         id: ${user1.id}
         login: ${user1.login}
         password: ${user1.password}
         mail: ${user1.mail}
-    """.trimIndent())
+    """.trimIndent()
+    )
 
-    println("""
+    println(
+        """
         id: ${user2.id}
         login: ${user2.login}
         password: ${user2.password}
         mail: ${user2.mail}
-    """.trimIndent())
+    """.trimIndent()
+    )
 }
