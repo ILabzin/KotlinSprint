54f54cf280dfe01f435b2b154323a6602ed029fe
Иван <Lbzin@bk.ru>
2024-12-06 14:56:19 +0300

    KS-13-3 setUser

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_13
+
+class User7(
+    val name: String,
+    val number: Long,
+    val company: String?,
+) {
+
+    fun printUser() {
+        val companyInText = company ?: "<не указано>"
+        println("-Имя:$name \n-Номер: $number \n-Компания: $companyInText")
+    }
+}
+
+fun main() {
+    val userSet = mutableSetOf<User7>(
+        User7("Vova", 547674858, "Poppi"),
+        User7("Viva", 547674859, "Poppi"),
+        User7("Vila", 547674857, "null"),
+        User7("Pila", 547674856, null),
+        User7("Poila", 547674855, null),
+    )
+    userSet.forEach() {
+        it.printUser()
+    }
+}
