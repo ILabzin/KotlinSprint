235eeb0e7ab34775671164a0073f0e2713b239cc
Иван <Lbzin@bk.ru>
2024-12-08 19:35:46 +0300

    KS-13-3 delete null-company in print

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -3,7 +3,7 @@ package org.example.lesson_13
 class User7(
     val name: String,
     val number: Long,
-    val company: String?,
+    val company: String? = null,
 ) {
 
     fun printUser() {
@@ -17,10 +17,15 @@ fun main() {
         User7("Vova", 547674858, "Poppi"),
         User7("Viva", 547674859, "Poppi"),
         User7("Vila", 547674857, "null"),
-        User7("Pila", 547674856, null),
-        User7("Poila", 547674855, null),
+        User7("Pila", 547674856),
+        User7("Poila", 547674855),
     )
-    userSet.forEach() {
+
+
+    val userWithCompany =
+        userSet.mapNotNull { if ((it.company.isNullOrEmpty()) || (it.company == "null")) null else it }
+
+    userWithCompany.forEach() {
         it.printUser()
     }
 }
