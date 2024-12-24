997dd96776a71ccbc5725b1a97d1c6ee719e5772
Иван <Lbzin@bk.ru>
2024-12-24 23:54:53 +0300

    KS-17-4 Figure

---
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_18
+
+abstract class Box(val height: Int) {
+    abstract fun calculateArea(): Int
+}
+
+class Rectangle(height: Int, val width: Int, val length: Int) : Box(height) {
+    override fun calculateArea(): Int {
+        return (height * width * length)
+    }
+}
+
+class Cube(height: Int) : Box(height) {
+    override fun calculateArea(): Int {
+        return (height * height * height)
+    }
+}
+
+fun main() {
+    val box1 = Rectangle(2,3,4)
+    val box2 = Cube(15)
+
+    println(box1.calculateArea())
+    println(box2.calculateArea())
+}
