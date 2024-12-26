a82f784197cb9eb5d6abf56bf2ee9e48b3778ebc
Иван <Lbzin@bk.ru>
2024-12-26 17:58:10 +0300

    KS-19-3 TODO

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_19
+
+class CosmoShip () {
+    fun rise() {
+        TODO("Написать логику")
+    }
+
+    fun land() {
+        TODO("Написать логику")
+    }
+
+    fun shoot() {
+        TODO("Написать логику")
+    }
+}
+
+fun main() {
+    val ship = CosmoShip()
+    ship.land()
+}
