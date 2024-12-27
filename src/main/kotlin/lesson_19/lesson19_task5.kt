9e74e1ebf9125fd2c1fb56883b377c4807ba87a7
Иван <Lbzin@bk.ru>
2024-12-27 19:05:56 +0300

    KS-19-5 add ERROR

--- src/main/kotlin/lesson_19/lesson19_task5.kt
+++ src/main/kotlin/lesson_19/lesson19_task5.kt
@@ -3,6 +3,7 @@ package org.example.lesson_19
 enum class Gender(val value: String) {
     MALE("Мужчина"),
     FEMALE("Женщина"),
+    ERROR("Ошибка"),
 }
 
 class People(
@@ -20,15 +21,20 @@ fun main() {
     var genderInString: String
     var gender: Gender
     val fileCabinet = mutableSetOf<People>()
+
     println("Добавим людей в картотеку")
+
     for (i in diaOfPeople) {
         println("Введите имя человека.")
         peopleName = readln()
         println("Введите пол человека. Доступно на выбор:")
         Gender.entries.forEach { println(it.value) }
         genderInString = readln()
-        gender = if (genderInString == Gender.MALE.value) Gender.MALE
-        else Gender.FEMALE
+        gender = when (genderInString) {
+            Gender.MALE.value -> Gender.MALE
+            Gender.FEMALE.value -> Gender.FEMALE
+            else -> Gender.ERROR
+        }
 
         fileCabinet.add(People(peopleName, gender))
     }
