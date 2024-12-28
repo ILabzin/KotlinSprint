bc7660f2b16b71a1ff8ae652103b0ba8035865fe
Иван <Lbzin@bk.ru>
2024-12-28 22:58:08 +0300

    KS-21-2 filter

--- src/main/kotlin/lesson_21/lesson21-task2.kt
+++ src/main/kotlin/lesson_21/lesson21-task2.kt
@@ -1,14 +1,10 @@
 package org.example.lesson_21
 
-fun List<Int>.evenNumbersSum():Int {
-    var sum =0
-    this.forEach {
-        if (it%2==0) sum +=it
-    }
-    return sum
+fun List<Int>.evenNumbersSum(): Int {
+    return this.filter { it % 2 == 0 }.sum()
 }
 
 fun main() {
-    val list1 = listOf<Int>(2,3,4,5,56,6,7,7,7)
+    val list1 = listOf<Int>(2, 3, 4, 5, 56, 6, 7, 7, 7)
     println(list1.evenNumbersSum())
 }
