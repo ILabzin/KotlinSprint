00ac7b11f06341bc8a18c783e711186e7c6b07dd
Иван <Lbzin@bk.ru>
2024-12-28 22:49:45 +0300

    KS-21-1 count

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -2,14 +2,8 @@ package org.example.lesson_21
 
 fun String.vowelCount(): Int {
     val stringToCharArray = this.toCharArray()
-    val ref: String = "aeiouAEIOU"
-    var count = 0
-    for (element in stringToCharArray)
-
-        if (ref.indexOf(element) != -1) {
-            count++
-        }
-    return count
+    val vowels: String = "aeiouAEIOU"
+   return this.count { it in vowels }
 }
 
 fun main() {
